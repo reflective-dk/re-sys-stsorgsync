c5b666ac2bd78be61ed65da5c383efb57838cff0
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2020-09-14 14:21:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var models = require('re-models');
+var path = require('path');
+var rootLoc = path.dirname(require.resolve('./index.js'));
+var confLoc = path.join(rootLoc, 'conf');
+var dataLoc = path.join(rootLoc, 'data');
+var miscLoc = path.join(rootLoc, 'misc');
+var docsLoc = path.join(rootLoc, 'docs');
+
+var confUtil = require('re-conf-util');
+
+module.exports = confUtil.prepareConf(confLoc, dataLoc, 'conf-stsorgsync', [ models ], miscLoc, docsLoc);
